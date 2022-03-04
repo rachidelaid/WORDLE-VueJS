4d9c2faf4638b283f198844b7fea6675d6ab868d
rachid elaid <rachidelaid@gmail.com>
2022-03-04 16:29:19 +0100

    toggle modal

--- src/components/ThemeModal.vue
+++ src/components/ThemeModal.vue
@@ -1,13 +1,21 @@
 <template>
-  <svg style="width: 24px; height: 24px" viewBox="0 0 24 24">
+  <svg
+    style="width: 24px; height: 24px"
+    viewBox="0 0 24 24"
+    @click="showModal = true"
+  >
     <path
       fill="currentColor"
       d="M17.5,12A1.5,1.5 0 0,1 16,10.5A1.5,1.5 0 0,1 17.5,9A1.5,1.5 0 0,1 19,10.5A1.5,1.5 0 0,1 17.5,12M14.5,8A1.5,1.5 0 0,1 13,6.5A1.5,1.5 0 0,1 14.5,5A1.5,1.5 0 0,1 16,6.5A1.5,1.5 0 0,1 14.5,8M9.5,8A1.5,1.5 0 0,1 8,6.5A1.5,1.5 0 0,1 9.5,5A1.5,1.5 0 0,1 11,6.5A1.5,1.5 0 0,1 9.5,8M6.5,12A1.5,1.5 0 0,1 5,10.5A1.5,1.5 0 0,1 6.5,9A1.5,1.5 0 0,1 8,10.5A1.5,1.5 0 0,1 6.5,12M12,3A9,9 0 0,0 3,12A9,9 0 0,0 12,21A1.5,1.5 0 0,0 13.5,19.5C13.5,19.11 13.35,18.76 13.11,18.5C12.88,18.23 12.73,17.88 12.73,17.5A1.5,1.5 0 0,1 14.23,16H16A5,5 0 0,0 21,11C21,6.58 16.97,3 12,3Z"
     />
   </svg>
-  <div class="modal-wrapper">
+  <div class="modal-wrapper" v-if="showModal">
     <div class="modal">
-      <svg style="width: 24px; height: 24px" viewBox="0 0 24 24">
+      <svg
+        style="width: 24px; height: 24px"
+        viewBox="0 0 24 24"
+        @click="showModal = false"
+      >
         <path
           fill="currentColor"
           d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z"
@@ -43,6 +51,8 @@ import { ref } from '@vue/reactivity';
 export default {
   name: 'ThemeModal',
   setup() {
+    const showModal = ref(false);
+
     const selectedIndex = ref(
       JSON.parse(localStorage.getItem('themeIndex')) || '0',
     );
@@ -89,7 +99,7 @@ export default {
       );
     };
 
-    return { themes, selectedIndex, selectTheme };
+    return { themes, selectedIndex, showModal, selectTheme };
   },
 };
 </script>
